50ce373250c2d989f36fc00135e0cbfaf3161835
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-08-02 19:29:26 -0400

    All tests passing, function implemented

--- ransom_note.py
+++ ransom_note.py
@@ -10,4 +10,21 @@ def can_construct(ransomNote: str, magazine: str) -> bool:
     Returns:
         bool: True if ransomNote can be constructed, False otherwise.
     """
-    pass  # TODO: Implement this function
+    mag_counts = {}
+    for char in magazine:
+        mag_counts[char] = mag_counts.get(char, 0) + 1
+    
+    for char in ransomNote:
+        if mag_counts.get(char, 0) == 0:
+            return False
+        mag_counts[char] -= 1
+
+    return True
+
+
+# Create an empty mag_counts dict
+# Loop through magazine and count each letter
+# Loop thorugh ransomNote:
+    # If the letter is not in mag_counts or count is 0 -> return False
+    # Otherwise, decrement the count in mag_counts
+# Return True if all letters were accounted for.
